6754697ce1190984580988931201fca2e9077004
neetelite <neetelite@protonmail.com>
2021-03-04 21:54:48 +0200

    I had messed up the install file by not specifying full paths. Fixed now!

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 echo "Installing dotfiles"
 
 echo "Copying repositories"
-ln -s -f ./.config ~/.config
-ln -s -f ./.screenlayout ~/.screenlayout
+ln -s -f ~/.dotfiles/.config ~/.config
+ln -s -f ~/.dotfiles/.screenlayout ~/.screenlayout
 
 echo "Copying files"
-ln -s -f ./.bashrc ~/.bashrc
-ln -s -f ./.vimrc ~/.vimrc
-ln -s -f ./.emacs ~/.emacs
-ln -s -f ./.xinitrc ~/.xinitrc
-ln -s -f ./.Xresources ~/.Xresources
+ln -s -f ~/.dotfiles/.bashrc ~/.bashrc
+ln -s -f ~/.dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/.dotfiles/.emacs ~/.emacs
+ln -s -f ~/.dotfiles/.xinitrc ~/.xinitrc
+ln -s -f ~/.dotfiles/.Xresources ~/.Xresources
 
 #TODO: Copy xorg.conf to the right place
 
